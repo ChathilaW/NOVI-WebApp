f4040dbe0bd3b91e637c36b146f4e59f7ea2952b
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2025-12-26 00:29:16 +0530

    added previous, recordings parts

--- novi-app/app/(main)/(home)/recordings/page.tsx
+++ novi-app/app/(main)/(home)/recordings/page.tsx
@@ -1,9 +1,13 @@
+import CallList from "@/components/CallList";
+
 const RecordingsPage = () => {
     return (
-        <section className="flex size-full flex-col gap-10 pt-20 text-white animate-fade-in">
-            <h1 className="text-3xl text-black text-center mt-3">Under Development....</h1>
+        <section className="flex size-full flex-col gap-10 text-white animate-fade-in">
+            <h1 className="text-3xl text-black text-center mt-3">Call Recordings</h1>
+
+            <CallList type="recordings" />
         </section>
-    )
+    );
 }
 
 export default RecordingsPage
